ad28874e2e5c5f15bf559a76f67f43fe7d1ddbe6
Adriel Amadi <adrielamadi7@gmail.com>
2024-09-23 01:47:16 +0100

    first commit

---
+++ src/Logo/Logo.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./Logo.css";
+
+const Logo = (props) => {
+  return (
+    <div className="home_b">
+      <Link to="/">
+    
+        <span className="span">TIMART </span>
+        </Link>
+      </div>
+
+  );
+};
+
+export default Logo;
